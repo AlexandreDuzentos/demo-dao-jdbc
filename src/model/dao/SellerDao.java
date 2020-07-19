5c2f37ee08b9b3adcf75f24313b0e933ab4a0098
AlexandreDuzentos <60664871+AlexandreDuzentos@users.noreply.github.com>
2020-07-19 21:19:51 +0100

    DepartmentDao and SellerDao interfaces

---
+++ src/model/dao/SellerDao.java
@@ -0,0 +1,13 @@
+package model.dao;
+
+import java.util.List;
+
+
+import Db.Seller;
+
+public interface SellerDao {
+ public void insert(Seller obj);
+ public void update(Seller obj);
+ public void deleteById(Integer id);
+ List<Seller>findAll();
+}
